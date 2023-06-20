160c8f9ad4826256d6b07ace2cf856e2e071d3ff
Nick Fox-Gieg <nick@fox-gieg.com>
2023-06-20 13:26:07 -0400

    first

---
+++ Scripts/LatkHideMenuTiltFive.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LatkHideMenuTiltFive : MonoBehaviour {
+
+	public TiltFive_NewController[] ctl;
+	public Renderer[] ren;
+
+	private bool firstRun = true;
+	private bool show = true; 
+
+	void Start() {
+		showHide(true);
+	}
+
+	void Update () {
+		if (firstRun) {
+			for (int i = 0; i < ctl.Length; i++) {
+				if (Input.GetMouseButtonDown(0) || Input.anyKeyDown || ctl[i].triggerDown || ctl[i].menuDown || ctl[i].gripDown || ctl[i].padDown) {
+					showHide(false);
+					firstRun = false;
+				}
+			}
+		} else if (Input.GetKeyDown (KeyCode.I)) {
+			showHide(!show);
+		}
+	}
+
+	void showHide(bool b) {
+		show = b;
+		for (int j = 0; j < ren.Length; j++) {
+			ren [j].enabled = b;
+		}
+	}
+
+}
---
+++ Scripts/LatkInputTiltFive.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LatkInputTiltFive : MonoBehaviour {
+
+
+	public LightningArtist latk;
+
+	private void Awake() {
+		if (latk == null) latk = GetComponent<LightningArtist>();
+	}
+
+	public void clickOn()
+    {
+		latk.clicked = true;
+    }
+
+	public void clickOff()
+    {
+		latk.clicked = false;
+    }
+
+}
